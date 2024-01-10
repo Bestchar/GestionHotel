7cc140c07f4b2e075248a7993485d300324871ef
christ972 <151399846+christ972@users.noreply.github.com>
2024-01-10 20:35:52 +0000

    Update Chambre.java

--- src/Controlleur/Chambre.java
+++ src/Controlleur/Chambre.java
@@ -14,5 +14,57 @@ public class Chambre {
     private String Caracteristique;
     private String Situation;
     private String Type;
+       public Chambre() {
+    }
+
+    public Chambre(int num_Chambre, String tp_Chambre, String Caracteristique, String Situation, String Type) {
+        this.num_Chambre = num_Chambre;
+        this.tp_Chambre = tp_Chambre;
+        this.Caracteristique = Caracteristique;
+        this.Situation = Situation;
+        this.Type = Type;
+    }
+    
+
+    public int getNum_Chambre() {
+        return num_Chambre;
+    }
+
+    public void setNum_Chambre(int num_Chambre) {
+        this.num_Chambre = num_Chambre;
+    }
+
+    public String getTp_Chambre() {
+        return tp_Chambre;
+    }
+
+    public void setTp_Chambre(String tp_Chambre) {
+        this.tp_Chambre = tp_Chambre;
+    }
+
+    public String getCaracteristique() {
+        return Caracteristique;
+    }
+
+    public void setCaracteristique(String Caracteristique) {
+        this.Caracteristique = Caracteristique;
+    }
+
+    public String getSituation() {
+        return Situation;
+    }
+
+    public void setSituation(String Situation) {
+        this.Situation = Situation;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String Type) {
+        this.Type = Type;
+    }
+    
     
 }
