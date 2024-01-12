6d91162d129dea03dedf4a1e4f31086c1c533df0
Bestchar <137408048+Bestchar@users.noreply.github.com>
2024-01-12 19:31:02 +0000

    Add files via upload

--- Gestion_Hotel.sql
+++ Gestion_Hotel.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1deb1
+-- version 5.2.1deb2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Nov 02, 2023 at 10:39 PM
--- Server version: 10.11.4-MariaDB-1
--- PHP Version: 8.2.7
+-- Hôte : localhost
+-- Généré le : sam. 06 jan. 2024 à 08:35
+-- Version du serveur : 10.11.6-MariaDB-1
+-- Version de PHP : 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `Gestion_Hotel`
+-- Base de données : `Gestion_Hotel`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `carte_de_Fidelite`
+-- Structure de la table `carte_de_Fidelite`
 --
 
 CREATE TABLE `carte_de_Fidelite` (
@@ -38,38 +38,56 @@ CREATE TABLE `carte_de_Fidelite` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `chambres`
+-- Structure de la table `Chambres`
 --
 
-CREATE TABLE `chambres` (
-  `id_chambre` int(200) NOT NULL,
-  `categorie` varchar(40) DEFAULT NULL,
-  `capacite` varchar(10) DEFAULT NULL,
-  `prix` int(100) DEFAULT NULL,
-  `id_Type_chambre` int(200) DEFAULT NULL
+CREATE TABLE `Chambres` (
+  `id_chambre` int(11) NOT NULL,
+  `type` varchar(20) DEFAULT NULL,
+  `caracteristique` varchar(20) DEFAULT NULL,
+  `situation` varchar(20) DEFAULT NULL,
+  `tarif` decimal(10,2) DEFAULT NULL,
+  `NUM_Chambre` int(11) DEFAULT NULL,
+  `Etat_Chambre` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `clients`
+-- Structure de la table `clients`
 --
 
 CREATE TABLE `clients` (
-  `id_client` int(11) NOT NULL,
+  `id_client` int(200) NOT NULL,
   `nom` varchar(40) DEFAULT NULL,
   `prenom` varchar(40) DEFAULT NULL,
   `origne` varchar(40) DEFAULT NULL,
   `numero_de_telephone` int(20) DEFAULT NULL,
-  `photo` varchar(255) DEFAULT NULL,
-  `id_carte_de_fidelite` int(11) DEFAULT NULL,
-  `id_Type_clients` int(11) DEFAULT NULL
+  `photo` longblob DEFAULT NULL,
+  `id_carte_de_fidelite` int(200) DEFAULT NULL,
+  `Categorie` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `clients`
+--
+
+INSERT INTO `clients` (`id_client`, `nom`, `prenom`, `origne`, `numero_de_telephone`, `photo`, `id_carte_de_fidelite`, `Categorie`) VALUES
+(1, 'edonvf ', 'jk lk', 'GHANA', 565689, NULL, NULL, NULL),
+(3, 'ch', 'cvv', 'TOGO', 45556, NULL, NULL, NULL),
+(4, 'dg', 'cvv', 'BENIN', 55465, NULL, NULL, NULL),
+(5, 'dg', 'cvv', 'BENIN', 55465, NULL, NULL, NULL),
+(6, 'fm [pm\'', 'fdf', 'BENIN', 498, NULL, NULL, NULL),
+(7, 'cb', 'cv', 'TOGO', 4422, NULL, NULL, NULL),
+(9, '', '', 'Choisir', 4, NULL, NULL, 'Choisir'),
+(10, 'kbhi', 'oj ', 'Choisir', 4, NULL, NULL, 'Choisir'),
+(14, 'kbhi', 'oj ', 'TOGO', 4, NULL, NULL, 'Clients groupes'),
+(16, 'mm', 'l', 'TOGO', 45, NULL, NULL, 'Clients privés,');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Facture`
+-- Structure de la table `Facture`
 --
 
 CREATE TABLE `Facture` (
@@ -83,14 +101,13 @@ CREATE TABLE `Facture` (
   `taux_remise_fidélité` decimal(5,2) DEFAULT NULL,
   `mode_paiement` varchar(20) DEFAULT NULL,
   `montant_payé` decimal(10,2) DEFAULT NULL,
-  `note` varchar(255) DEFAULT NULL,
-  `Chambre_id` int(11) DEFAULT NULL
+  `note` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Facture_Service`
+-- Structure de la table `Facture_Service`
 --
 
 CREATE TABLE `Facture_Service` (
@@ -105,21 +122,23 @@ CREATE TABLE `Facture_Service` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `reservation`
+-- Structure de la table `reservation`
 --
 
 CREATE TABLE `reservation` (
   `id_reservation` int(11) NOT NULL,
   `id_client` int(11) DEFAULT NULL,
-  `id_chambre` int(11) DEFAULT NULL,
+  `id_chambres` int(11) DEFAULT NULL,
   `date_d_arrive` date DEFAULT NULL,
-  `date_depart` date DEFAULT NULL
+  `date_depart` date DEFAULT NULL,
+  `date_reser` date DEFAULT NULL,
+  `validiter` int(25) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Service`
+-- Structure de la table `Service`
 --
 
 CREATE TABLE `Service` (
@@ -129,40 +148,13 @@ CREATE TABLE `Service` (
   `prix` decimal(10,2) DEFAULT NULL,
   `disponibilite` tinyint(1) DEFAULT NULL,
   `heures_ouverture` varchar(255) DEFAULT NULL,
-  `emplacement` varchar(255) DEFAULT NULL,
-  `type_service` varchar(255) DEFAULT NULL,
-  `evaluations` text DEFAULT NULL,
-  `commentaires` text DEFAULT NULL
+  `type_service` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Type_chambre`
---
-
-CREATE TABLE `Type_chambre` (
-  `id_Type_chambre` int(200) NOT NULL,
-  `categorie_chambre` text DEFAULT NULL,
-  `description_chambre` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Type_clients`
---
-
-CREATE TABLE `Type_clients` (
-  `id_Type_clients` int(40) NOT NULL,
-  `categories` tinytext DEFAULT NULL,
-  `description` tinytext DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Utilisateurs`
+-- Structure de la table `Utilisateurs`
 --
 
 CREATE TABLE `Utilisateurs` (
@@ -174,165 +166,138 @@ CREATE TABLE `Utilisateurs` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Indexes for dumped tables
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `carte_de_Fidelite`
+-- Index pour la table `carte_de_Fidelite`
 --
 ALTER TABLE `carte_de_Fidelite`
   ADD PRIMARY KEY (`id_carte`);
 
 --
--- Indexes for table `chambres`
+-- Index pour la table `Chambres`
 --
-ALTER TABLE `chambres`
+ALTER TABLE `Chambres`
   ADD PRIMARY KEY (`id_chambre`);
 
 --
--- Indexes for table `clients`
+-- Index pour la table `clients`
 --
 ALTER TABLE `clients`
-  ADD PRIMARY KEY (`id_client`),
-  ADD KEY `id_carte_de_fidelite` (`id_carte_de_fidelite`),
-  ADD KEY `id_Type_clients` (`id_Type_clients`);
+  ADD PRIMARY KEY (`id_client`);
 
 --
--- Indexes for table `Facture`
+-- Index pour la table `Facture`
 --
 ALTER TABLE `Facture`
   ADD PRIMARY KEY (`id_facture`),
   ADD KEY `id_client` (`id_client`),
-  ADD KEY `id_reservation` (`id_reservation`),
-  ADD KEY `Chambre_id` (`Chambre_id`);
+  ADD KEY `id_reservation` (`id_reservation`);
 
 --
--- Indexes for table `Facture_Service`
+-- Index pour la table `Facture_Service`
 --
 ALTER TABLE `Facture_Service`
   ADD PRIMARY KEY (`id_facture_service`),
   ADD KEY `id_facture` (`id_facture`);
 
 --
--- Indexes for table `reservation`
+-- Index pour la table `reservation`
 --
 ALTER TABLE `reservation`
   ADD PRIMARY KEY (`id_reservation`),
+  ADD KEY `id_chambres` (`id_chambres`),
   ADD KEY `id_client` (`id_client`);
 
 --
--- Indexes for table `Service`
+-- Index pour la table `Service`
 --
 ALTER TABLE `Service`
   ADD PRIMARY KEY (`id_service`);
 
 --
--- Indexes for table `Type_chambre`
---
-ALTER TABLE `Type_chambre`
-  ADD PRIMARY KEY (`id_Type_chambre`);
-
---
--- Indexes for table `Type_clients`
---
-ALTER TABLE `Type_clients`
-  ADD PRIMARY KEY (`id_Type_clients`);
-
---
--- Indexes for table `Utilisateurs`
+-- Index pour la table `Utilisateurs`
 --
 ALTER TABLE `Utilisateurs`
   ADD PRIMARY KEY (`id_utilisateur`),
   ADD UNIQUE KEY `nom_utilisateur` (`nom_utilisateur`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `carte_de_Fidelite`
+-- AUTO_INCREMENT pour la table `carte_de_Fidelite`
 --
 ALTER TABLE `carte_de_Fidelite`
   MODIFY `id_carte` int(200) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `chambres`
+-- AUTO_INCREMENT pour la table `Chambres`
 --
-ALTER TABLE `chambres`
-  MODIFY `id_chambre` int(200) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `Chambres`
+  MODIFY `id_chambre` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `clients`
+-- AUTO_INCREMENT pour la table `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `id_client` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_client` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
--- AUTO_INCREMENT for table `Facture`
+-- AUTO_INCREMENT pour la table `Facture`
 --
 ALTER TABLE `Facture`
   MODIFY `id_facture` int(200) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `Facture_Service`
+-- AUTO_INCREMENT pour la table `Facture_Service`
 --
 ALTER TABLE `Facture_Service`
   MODIFY `id_facture_service` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `reservation`
+-- AUTO_INCREMENT pour la table `reservation`
 --
 ALTER TABLE `reservation`
   MODIFY `id_reservation` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `Service`
+-- AUTO_INCREMENT pour la table `Service`
 --
 ALTER TABLE `Service`
   MODIFY `id_service` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `Type_chambre`
---
-ALTER TABLE `Type_chambre`
-  MODIFY `id_Type_chambre` int(200) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `Type_clients`
---
-ALTER TABLE `Type_clients`
-  MODIFY `id_Type_clients` int(40) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `Utilisateurs`
+-- AUTO_INCREMENT pour la table `Utilisateurs`
 --
 ALTER TABLE `Utilisateurs`
   MODIFY `id_utilisateur` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- Constraints for dumped tables
+-- Contraintes pour les tables déchargées
 --
 
 --
--- Constraints for table `clients`
---
-ALTER TABLE `clients`
-  ADD CONSTRAINT `clients_ibfk_1` FOREIGN KEY (`id_carte_de_fidelite`) REFERENCES `clients` (`id_client`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `clients_ibfk_2` FOREIGN KEY (`id_Type_clients`) REFERENCES `clients` (`id_client`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `Facture`
+-- Contraintes pour la table `Facture`
 --
 ALTER TABLE `Facture`
   ADD CONSTRAINT `Facture_ibfk_1` FOREIGN KEY (`id_client`) REFERENCES `clients` (`id_client`),
-  ADD CONSTRAINT `Facture_ibfk_2` FOREIGN KEY (`id_reservation`) REFERENCES `reservation` (`id_reservation`),
-  ADD CONSTRAINT `Facture_ibfk_3` FOREIGN KEY (`Chambre_id`) REFERENCES `chambres` (`id_chambre`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `Facture_ibfk_2` FOREIGN KEY (`id_reservation`) REFERENCES `reservation` (`id_reservation`);
 
 --
--- Constraints for table `Facture_Service`
+-- Contraintes pour la table `Facture_Service`
 --
 ALTER TABLE `Facture_Service`
   ADD CONSTRAINT `Facture_Service_ibfk_1` FOREIGN KEY (`id_facture`) REFERENCES `Facture` (`id_facture`);
+
+--
+-- Contraintes pour la table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`id_chambres`) REFERENCES `reservation` (`id_reservation`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`id_client`) REFERENCES `reservation` (`id_reservation`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
